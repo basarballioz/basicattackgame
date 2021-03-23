814a15b46019f839b4119c005c497f5a33a3ad28
Basar Ballioz <balliozbasar@gmail.com>
2021-03-23 21:48:16 +0300

    vue js added

---
+++ app.js
@@ -0,0 +1,25 @@
+new Vue({
+    el: '#app',
+    data: {
+        playerHP: 100,
+        opforHP: 100,
+        startGame: false
+    },
+    methods: {
+        startGame: function() {
+            
+        },
+        attack: function() {
+            
+        },
+        finisherAttack: function() {
+            
+        },
+        firstAid: function() {
+            
+        },
+        surrender: function() {
+            
+        }
+    }
+})
