dc865c7f5f2616fda231abe8c4fb1bece9278391
Basar Ballioz <balliozbasar@gmail.com>
2021-03-23 22:20:30 +0300

    buttons are coded

--- app.js
+++ app.js
@@ -3,23 +3,31 @@ new Vue({
     data: {
         playerHP: 100,
         opforHP: 100,
-        startGame: false
+        newGame: false
     },
     methods: {
         startGame: function() {
-            
+            this.newGame = true;
         },
         attack: function() {
-            
+            var dmg = Math.ceil(Math.random() * 15);
+            this.opforHP -= dmg;
+            this.autoAttack();
         },
         finisherAttack: function() {
-            
+            var dmg = Math.ceil(Math.random() * 20 );
+            this.opforHP -= dmg;
+            this.autoAttack();
         },
         firstAid: function() {
             
         },
         surrender: function() {
             
+        },
+        autoAttack: function() {
+            var dmg = Math.ceil(Math.random() * 15);
+            this.playerHP -= dmg;
         }
     }
 })
